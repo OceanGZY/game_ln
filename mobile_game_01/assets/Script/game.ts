d07cb7234f989897df6490ad4260ac85eacf56c6
OcaenEyes <1450136519@qq.com>
2021-08-15 22:16:36 +0800

    master

--- mobile_game_01/assets/Script/game.ts
+++ mobile_game_01/assets/Script/game.ts
@@ -5,24 +5,29 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
+  @property(cc.Label)
+  label: cc.Label = null;
 
-    @property(cc.Label)
-    label: cc.Label = null;
+  @property
+  text: string = "hello";
 
-    @property
-    text: string = 'hello';
+  @property(cc.Label)
+  label1: cc.Label = null;
 
-    // LIFE-CYCLE CALLBACKS:
+  @property
+  text1: string = "asda";
+  // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+  onLoad() {
+    this.label.string = this.text;
+    this.label1.string = this.text1;
+  }
 
-    start () {
+  start() {}
 
-    }
-
-    // update (dt) {}
+  // update (dt) {}
 }
