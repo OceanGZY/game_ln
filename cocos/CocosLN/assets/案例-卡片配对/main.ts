184bc64c9eefff4c2767e877bc90c71c4eab8cf2
OcaenEyes <1450136519@qq.com>
2024-02-05 15:15:44 +0800

    update 将卡牌配对优化为promise await 优雅的模式

--- cocos/CocosLN/assets/案例-卡片配对/main.ts
+++ cocos/CocosLN/assets/案例-卡片配对/main.ts
@@ -2,7 +2,7 @@
  * @Author: OCEAN.GZY
  * @Date: 2024-02-04 23:12:26
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2024-02-05 10:11:51
+ * @LastEditTime: 2024-02-05 15:09:08
  * @FilePath: /CocosLN/assets/案例-卡片配对/main.ts
  * @Description: 注释信息
  */
@@ -24,14 +24,24 @@ export class main extends Component {
         id: -1
     };
 
-    start() {
-        this.orderAllCards();
-        this.createAllCards();
-        this.moveAllCards();
+    async start() {
+        // 洗牌与创建卡牌是同步代码
+        this.orderAllCards(); // 洗牌过程
+        this.createAllCards(); // 创建卡 牌过程
+        // this.setCardToPosInitial();  // 将卡牌放置到对应位置
+        await this.moveAllCards();
+        await this.setCardToBackAnimation();
+        this.addCardsEvent();
     }
 
     update(deltaTime: number) {
 
+    }
+
+    orderAllCards() {
+        this.cards.sort(() => 0.5 - Math.random());
+        this.cards.sort(() => 0.5 - Math.random());
+        this.cards.sort(() => 0.5 - Math.random());
     }
 
     createAllCards() {
@@ -53,20 +63,8 @@ export class main extends Component {
         sprite.spriteFrame = sf;
     }
 
-    orderAllCards() {
-        this.cards.sort(() => 0.5 - Math.random());
-        this.cards.sort(() => 0.5 - Math.random());
-        this.cards.sort(() => 0.5 - Math.random());
-    }
-
-    moveAllCards() {
-        this.node.children.forEach((node, index) => { // 铺牌，并发到各个位置
-            const posX = this.pointRoot.children[index].position.x;
-            const posY = this.pointRoot.children[index].position.y;
-            // node.setPosition(posX, posY);
-            tween(node).delay(index * 0.1).to(0.5, { position: v3(posX, posY, 1) }).start();
-        });
-        this.scheduleOnce(() => { // 模拟翻牌效果
+    setCardToBackAnimation() {// 模拟翻牌效果
+        return new Promise<void>(resolve => {
             this.node.children.forEach((node, index) => {
                 tween(node)
                     .to(0.5, { scale: v3(0, 1, 1) })
@@ -78,14 +76,43 @@ export class main extends Component {
                     .start()
             });
             this.scheduleOnce(() => {
-                this.addCardsEvent();
+                resolve();
+            }, 1);
+        });
+    }
+
+    moveAllCards() {
+        return new Promise<void>(resolve => {
+            this.node.children.forEach((node, index) => { // 铺牌，并发到各个位置
+                const posX = this.pointRoot.children[index].position.x;
+                const posY = this.pointRoot.children[index].position.y;
+                // node.setPosition(posX, posY);
+                tween(node).delay(index * 0.1).to(0.5, { position: v3(posX, posY, 1) }).start();
             });
-        }, 2);
+
+            this.scheduleOnce(() => {
+                resolve();
+            }, 2);
+        });
+    }
+
+    makeCardTurnAnimation(node: Node, isback: boolean, id?: TCardId) {
+        return new Promise<void>(resolve => {
+            tween(node)
+                .to(0.5, { scale: v3(0, 1, 1) })
+                .call(() => {
+                    const sprite = node.getComponent(Sprite);
+                    sprite.spriteFrame = isback ? this.imageManager.getCardBackSF() : this.imageManager.getCardSFById(id);
+                })
+                .to(0.5, { scale: v3(1, 1, 1) })
+                .call(() => resolve())
+                .start();
+        });
     }
 
     addCardsEvent() {
         this.node.children.forEach((node, index) => {
-            node.on(Node.EventType.TOUCH_END, (event: EventTouch) => {
+            node.on(Node.EventType.TOUCH_END, async (event: EventTouch) => {
 
                 if (node === this.currentOpenCrad.node) { // 卡牌已经被打开了
                     return;
@@ -94,14 +121,7 @@ export class main extends Component {
                 const id: TCardId = this.cards[index];
                 if (!this.currentOpenCrad.node) {
                     // 让被点击的卡牌反过来
-                    tween(node)
-                        .to(0.5, { scale: v3(0, 1, 1) })
-                        .call(() => {
-                            const sprite = node.getComponent(Sprite);
-                            sprite.spriteFrame = this.imageManager.getCardSFById(id);
-                        })
-                        .to(0.5, { scale: v3(1, 1, 1) })
-                        .start();
+                    await this.makeCardTurnAnimation(node, false, id);
                     this.currentOpenCrad.id = id;
                     this.currentOpenCrad.node = node;
                     console.log(this.currentOpenCrad);
@@ -109,53 +129,21 @@ export class main extends Component {
                     // 判断翻开的卡牌数值是否一样
                     if (id === this.currentOpenCrad.id) {
                         console.log("配对成功！ID是", id);
-                        tween(node)
-                            .to(0.5, { scale: v3(0, 1, 1) })
-                            .call(() => {
-                                const sprite = node.getComponent(Sprite);
-                                sprite.spriteFrame = this.imageManager.getCardSFById(id);
-                            })
-                            .to(0.5, { scale: v3(1, 1, 1) })
-                            .call(() => {
-                                // 配对成功后则两张卡牌消失
-                                this.currentOpenCrad.node.active = false;
-                                this.currentOpenCrad.node = null;
-                                this.currentOpenCrad.id = -1;
-                                node.active = false;
-                            })
-                            .start();
+                        await this.makeCardTurnAnimation(node, false, id);
+                        // 配对成功后则两张卡牌消失
+                        this.currentOpenCrad.node.active = false;
+                        this.currentOpenCrad.node = null;
+                        this.currentOpenCrad.id = -1;
+                        node.active = false;
+
                     } else {
                         console.log("配对失败！");
-                        tween(node)
-                            .to(0.5, { scale: v3(0, 1, 1) })
-                            .call(() => {
-                                const sprite = node.getComponent(Sprite);
-                                sprite.spriteFrame = this.imageManager.getCardSFById(id);
-                            })
-                            .to(0.5, { scale: v3(1, 1, 1) })
-                            .call(() => {
-                                tween(this.currentOpenCrad.node)
-                                    .to(0.5, { scale: v3(0, 1, 1) })
-                                    .call(() => {
-                                        const sprite = this.currentOpenCrad.node.getComponent(Sprite);
-                                        sprite.spriteFrame = this.imageManager.getCardBackSF();
-                                    })
-                                    .to(0.5, { scale: v3(1, 1, 1) })
-                                    .call(() => {
-                                        this.currentOpenCrad.node = null;
-                                        this.currentOpenCrad.id = -1;
-                                    })
-                                    .start();
-                                tween(node)
-                                    .to(0.5, { scale: v3(0, 1, 1) })
-                                    .call(() => {
-                                        const sprite = node.getComponent(Sprite);
-                                        sprite.spriteFrame = this.imageManager.getCardBackSF();
-                                    })
-                                    .to(0.5, { scale: v3(1, 1, 1) })
-                                    .start();
 
-                            }).start();
+                        await this.makeCardTurnAnimation(node, false, id);
+                        this.makeCardTurnAnimation(node, true);
+                        await this.makeCardTurnAnimation(this.currentOpenCrad.node, true);
+                        this.currentOpenCrad.node = null;
+                        this.currentOpenCrad.id = -1;
                     }
                 }
             }, this)
