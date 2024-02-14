a4cd46215f16fb03459437ca455bd8bf2c857f99
OcaenEyes <1450136519@qq.com>
2024-02-14 16:42:41 +0800

    update 修改鼠标，实现player武器 、攻击效果

--- godot/roguelike/character/player/player.gd
+++ godot/roguelike/character/player/player.gd
@@ -1,4 +1,10 @@
 extends BaseCharacter
+@onready var sword = get_node("Sword")
+@onready var sword_animation_player = sword.get_node("SwordAnimationPlayer")
+@onready var sword_slash_sprite = sword.get_node("SlashSprite")
+
+func _ready():
+	sword_slash_sprite.hide()
 
 func _process(delta):
 	var mouse_direction = (get_global_mouse_position()-global_position).normalized()
@@ -6,6 +12,19 @@ func _process(delta):
 		animated_sprite_2d.flip_h =false
 	elif mouse_direction.x<0 and not animated_sprite_2d.flip_h:
 		animated_sprite_2d.flip_h =true
+		
+	sword.rotation = mouse_direction.angle()
+	if sword.scale.y==1 and mouse_direction.x <0:
+		sword.scale.y=-1
+		#print("此",sword.scale)
+	elif sword.scale.y== -1 and mouse_direction.x >0:
+		sword.scale.y=1
+		#print("此时",sword.scale)
+		
+	if Input.is_action_pressed("ui_attack"):
+		sword_slash_sprite.show() 
+		if not sword_animation_player.is_playing():
+			sword_animation_player.play("attack")
 
 func get_input():
 	mov_direction =Vector2.ZERO
