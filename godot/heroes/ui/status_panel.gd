0baf60b0e2346bb867f3b7a7902173fe97421385
OcaenEyes <1450136519@qq.com>
2024-02-16 20:37:01 +0800

    实现血条底纹动画

--- godot/heroes/ui/status_panel.gd
+++ godot/heroes/ui/status_panel.gd
@@ -2,6 +2,7 @@ extends HBoxContainer
 
 @export var stats: Stats
 @onready var health_bar: TextureProgressBar = $HealthBar
+@onready var eased_health_bar: TextureProgressBar = $HealthBar/EasedHealthBar
 
 
 func _ready() -> void:
@@ -11,3 +12,5 @@ func _ready() -> void:
 func update_health()-> void:
 	var percentage:= stats.health / float(stats.max_health)
 	health_bar.value = percentage
+	
+	create_tween().tween_property(eased_health_bar,"value",percentage,0.3)
