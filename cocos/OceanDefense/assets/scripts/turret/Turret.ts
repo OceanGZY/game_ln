fb0bd6c4e284a34dda59ea051f17ae4cc05fcb8a
OceanGZY <1450136519@qq.com>
2024-07-27 11:57:25 +0800

    update cocos defense

--- cocos/OceanDefense/assets/scripts/turret/Turret.ts
+++ cocos/OceanDefense/assets/scripts/turret/Turret.ts
@@ -2,26 +2,38 @@
  * @Author: OCEAN.GZY
  * @Date: 2024-07-25 21:02:17
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2024-07-26 22:51:31
+ * @LastEditTime: 2024-07-27 11:53:06
  * @FilePath: \OceanDefense\assets\scripts\turret\Turret.ts
  * @Description: 注释信息
  */
-import { _decorator, Component, Node, Prefab } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Turret')
 export class Turret extends Component {
 
+
     @property(Prefab)
     bulletPrefab: Prefab = null;
 
+    firePoint: Node = null;
+
 
     start() {
-
+        this.firePoint = this.node.getChildByName("FirePoint");
+        this.schedule(() => {
+            this.fire();
+        },5)
     }
 
     update(deltaTime: number) {
 
+    }
+
+    fire() {
+        const temp = instantiate(this.bulletPrefab);
+        this.node.scene.getChildByName("BulletManager").addChild(temp);
+        temp.setPosition(this.firePoint.position);
     }
 }
 
